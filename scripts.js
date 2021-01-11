6d8ed893b9472f6cba982be1ec9870d00cef3839
dadiraip <dadiraip@gmail.com>
2021-01-11 22:19:48 +0200

    update

---
+++ scripts.js
@@ -0,0 +1,5 @@
+//Get Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+//Get last modified date
+document.getElementById("update").innerHTML = document.lastModified;
